3dabbcb3c95bc5683e21adf4c60cd9dea026db41
blessedmadukoma <blessedmadukoma@gmail.com>
2023-08-10 16:02:54 +0100

    project 1 done

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,59 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue"
+
+let count = ref(0);
+
+const addToCount = () => {
+  count.value++;
+  console.log(count.value);
+}
+
+const subtractFromCount = () => {
+  count.value--;
+}
 </script>
 
+<!-- <script>
+export default ({
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    addToCount() {
+      this.count.value++;
+    },
+    subtractFromCount() {
+      this.count.value--;
+    }
+  }
+})
+</script> -->
+
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <main>
+    <div>
+      <h4>The current count is...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="subtractFromCount">-</button>
+      <button @click="addToCount">+</button>
     </div>
-  </header>
-
-  <RouterView />
+  </main>
 </template>
 
+<!-- <style lang="scss"> -->
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+main {
+  background-color: aliceblue;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
+div {
   text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
